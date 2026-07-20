5a295f381bc4ab8284759f3b8ae1d458e7931561
tobiasreyeros <tobiasreyeros62@gmail.com>
2026-07-20 19:36:18 -0300

    Actualización del e15.py

--- e15.py
+++ e15.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image, ImageOps
 
-img = Image.open("e16_imagen")
+img = Image.open("zenith.jpg")
 matriz = np.array(img_g)
 aux = 0
 
