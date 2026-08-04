96167bc67366faaa14a71e234fd8b009b3e129e1
tobiasreyeros <tobiasreyeros62@gmail.com>
2026-08-04 08:17:49 -0300

    Actualizacion del e17.py
    Se le añadio la rotacion a 180°

--- e17.py
+++ e17.py
@@ -7,16 +7,27 @@ def rotacion(ruta_imagen):
     imagen=np.array(imagen)
     dimensiones=np.shape(imagen)
     imagen_rotada=[]
+    imagen_volteada=[]
     filas=dimensiones[0]
     colum=dimensiones[1]
+    #Rotacion 90°
     for j in range(colum):
         nueva_fila=[]
         for i in range(filas-1,-1,-1):
             nueva_fila.append(imagen[i][j])
         imagen_rotada.append(nueva_fila)
+     
+    #Rotacion 180° 
+    for i in range(filas - 1, -1, -1):
+        nueva_fila = []
+        for j in range(colum - 1, -1, -1):
+            nueva_fila.append(imagen[i][j])
+        imagen_volteada.append(nueva_fila)
     plt.imshow(imagen, cmap="grey")
     plt.show()
     plt.imshow(imagen_rotada, cmap="grey")
+    plt.show()
+    plt.imshow(imagen_volteada, cmap="grey")
     plt.show()
 
 rotacion("zenith.jpg")
